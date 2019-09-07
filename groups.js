2576e0d0828945480df27a4010fbc6ece7072ba5
AdministratorBopimo <52251206+AdministratorBopimo@users.noreply.github.com>
2019-09-07 22:18:58 +0100

    Create groups.js

---
+++ groups.js
@@ -0,0 +1,59 @@
+const botconfig = require("./settings.json")
+var items = require("./items.json")
+const Discord = require('discord.js');
+const colours = require("./colours")
+var fs = require('fs');
+
+const bot = new Discord.Client();
+const prefix = botconfig.prefix;
+const userprefix = botconfig.userprefix;
+//main
+bot.on("message", (message) => {
+
+if (message.content.startsWith(`${userprefix}` + "group")) {     
+    var parameters = message.content.split(" ");
+    var groupName = parameters[1];
+
+    //fetch
+    const request = require('request');
+    request.get(`https://brickplanet.com/web-api/groups/get-group/${groupName}`, (err, res, groupN) => {
+    let group = JSON.parse(groupN)
+    let name = group.Name
+    //verified
+    if (group.IsVerified == 1){
+        var verified = "<:verified:619911977728213052>"
+    }
+    else {
+        verified = "<:notadmin:619916084279115787>"
+    }
+    //main
+    if (group.msg == "Missing parameter(s)"){
+        let sEmbed = new Discord.RichEmbed()
+        .setColor(colours.gold)
+        .setAuthor("BrickPlanet+", items.author.icon, items.author.server)
+        .setTitle("User not found " + "<:notadmin:619916084279115787>")     
+        .setTimestamp()
+        .setFooter("Made by Administrator")
+        message.channel.send({embed: sEmbed});
+    }
+    else{
+        let sEmbed = new Discord.RichEmbed()
+        .setColor(colours.gold)
+        .setAuthor("BrickPlanet+", items.author.icon, items.author.server)
+        .setTitle(group.Name)
+        .addField("ID", group.ID)
+        .setURL("https://www.brickplanet.com/communities/" + groupName + "/" + group.Name)
+        .setThumbnail("https://cdn.brickplanet.com/" + group.Image)
+        //options
+        .addField("Member Count", group.MemberCount, true)
+        .addField("Verified",verified)
+        //bottm part
+        .setTimestamp()
+        .setFooter("Made by Administrator")
+        message.channel.send({embed: sEmbed});
+    }
+    });
+}
+});
+
+bot.login(botconfig.token);
